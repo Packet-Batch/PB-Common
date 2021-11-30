a5cfa6229d9a3774b314c5caf113c3502e8925b7
gamemann <gamemann@gflclan.com>
2021-11-30 18:52:14 +0000

    Add Command Line source/header files.

---
+++ src/cmd_line.c
@@ -0,0 +1,418 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "cmdline.h"
+#include "config.h"
+
+static struct option long_opts[] =
+{
+    {"cfg", required_argument, NULL, 'c'},
+    {"cli", no_argument, NULL, 'z'},
+
+    /* CLI options. */
+    {"interface", required_argument, NULL, 0},
+    {"send", required_argument, NULL, 1},
+    {"block", required_argument, NULL, 2},
+    {"count", required_argument, NULL, 3},
+    {"time", required_argument, NULL, 4},
+    {"delay", required_argument, NULL, 5},
+    {"trackcount", required_argument, NULL, 6},
+    {"data", required_argument, NULL, 7},
+    {"threads", required_argument, NULL, 8},
+    {"l4csum", required_argument, NULL, 9},
+
+    {"smac", required_argument, NULL, 10},
+    {"dmac", required_argument, NULL, 11},
+
+    {"minttl", required_argument, NULL, 12},
+    {"maxttl", required_argument, NULL, 13},
+    {"minid", required_argument, NULL, 14},
+    {"maxid", required_argument, NULL, 15},
+    {"srcip", required_argument, NULL, 16},
+    {"dstip", required_argument, NULL, 17},
+    {"protocol", required_argument, NULL, 18},
+    {"tos", required_argument, NULL, 19},
+    {"l3csum", required_argument, NULL, 20},
+
+    {"usport", required_argument, NULL, 21},
+    {"udport", required_argument, NULL, 22},
+
+    {"tsport", required_argument, NULL, 23},
+    {"tdport", required_argument, NULL, 24},
+    {"tsyn", required_argument, NULL, 25},
+    {"tack", required_argument, NULL, 26},
+    {"tpsh", required_argument, NULL, 27},
+    {"trst", required_argument, NULL, 28},
+    {"tfin", required_argument, NULL, 29},
+    {"turg", required_argument, NULL, 30},
+    {"tusecooked", required_argument, NULL, 31},
+
+    {"icode", required_argument, NULL, 38},
+    {"itype", required_argument, NULL, 39},
+
+    {"pmin", required_argument, NULL, 32},
+    {"pmax", required_argument, NULL, 33},
+    {"pstatic", required_argument, NULL, 34},
+    {"pexact", required_argument, NULL, 35},
+    {"pfile", required_argument, NULL, 36},
+    {"pstring", required_argument, NULL, 37},
+
+    {"list", no_argument, NULL, 'l'},
+    {"verbose", no_argument, NULL, 'v'},
+    {"help", no_argument, NULL, 'h'},
+    {NULL, 0, NULL, 0}
+};
+
+/**
+ * Parses CLI options if --cli is passed.
+ * 
+ * @param cmd The cmdline structure to grab the command line values from.
+ * @param cfg The config structure to save the command line values to.
+ * @param sequence
+ * 
+ * @return void
+**/
+void parse_cli(struct cmdline *cmd, struct config *cfg)
+{
+    /* Parse main options. */
+    if (cmd->cl_interface != NULL)
+    {
+        cfg->interface = cmd->cl_interface;
+    }
+
+    cfg->seq[0].block = cmd->cl_block;
+    cfg->seq[0].count = cmd->cl_count;
+    cfg->seq[0].time = cmd->cl_time;
+    cfg->seq[0].delay = cmd->cl_delay;
+    cfg->seq[0].track_count = cmd->cl_track_count;
+    cfg->seq[0].max_data = cmd->cl_max_data;
+    cfg->seq[0].threads = cmd->cl_threads;
+    cfg->seq[0].l4_csum = cmd->cl_l4_csum;
+
+    cfg->seq[0].eth.smac = cmd->cl_smac;
+    cfg->seq[0].eth.dmac = cmd->cl_dmac;
+
+    cfg->seq[0].ip.min_ttl = cmd->cl_ttl_min;
+    cfg->seq[0].ip.max_ttl = cmd->cl_ttl_max;
+    cfg->seq[0].ip.min_id = cmd->cl_id_min;
+    cfg->seq[0].ip.max_id = cmd->cl_id_max;
+
+    if (cmd->cl_src_ip != NULL)
+    {
+        // Check for range.
+        if (strstr(cmd->cl_src_ip, "/") != NULL)
+        {
+            cfg->seq[0].ip.src_ip = 0;
+            cfg->seq[0].ip.range_count = 1;
+            cfg->seq[0].ip.ranges[0] = cmd->cl_src_ip;
+        }
+        else
+        {
+            cfg->seq[0].ip.src_ip = cmd->cl_src_ip;
+        }
+    }
+
+    if (cmd->cl_dst_ip != NULL)
+    {
+        cfg->seq[0].ip.dstip = cmd->cldstip;
+    }
+
+    cfg->seq[0].ip.protocol = cmd->cl_protocol;
+    cfg->seq[0].ip.tos = cmd->cl_tos;
+    cfg->seq[0].ip.csum = cmd->cl_l3_csum;
+
+    cfg->seq[0].udp.src_port = cmd->cl_udp_src_port;
+    cfg->seq[0].udp.dst_port = cmd->cl_udp_dst_port;
+
+    cfg->seq[0].tcp.src_port = cmd->cl_tcp_src_port;
+    cfg->seq[0].tcp.dst_port = cmd->cl_tcp_dst_port;
+    cfg->seq[0].tcp.syn = cmd->cl_tcp_syn;
+    cfg->seq[0].tcp.ack = cmd->cl_tcp_ack;
+    cfg->seq[0].tcp.psh = cmd->cl_tcp_psh;
+    cfg->seq[0].tcp.rst = cmd->cl_tcp_rst;
+    cfg->seq[0].tcp.fin = cmd->cl_tcp_fin;
+    cfg->seq[0].tcp.urg = cmd->cl_tcp_urg;
+    cfg->seq[0].tcp.use_socket = cmd->cl_tcp_use_socket;
+
+    cfg->seq[0].icmp.code = cmd->cl_icmp_code;
+    cfg->seq[0].icmp.type = cmd->cl_icmp_type;
+
+    cfg->seq[0].pl.min_len = cmd->cl_pl_min_len;
+    cfg->seq[0].pl.max_len = cmd->cl_pl_max_len;
+    cfg->seq[0].pl.static_data = cmd->cl_pl_static_data;
+
+    if (cmd->cl_pl_exact != NULL)
+    {
+        cfg->seq[0].pl.exact = cmd->cl_pl_exact;
+    }
+
+    cfg->seq[0].pl.is_file = cmd->cl_pl_is_file;
+    cfg->seq[0].pl.is_string = cmd->cl_pl_is_string;
+}
+
+/**
+ * Parses the command line options using getopt.
+ * 
+ * @param argc The argument counter passed in the `int main()` function.
+ * @param argv The argument array pointer passed in the `int main()` function.
+ * @param cmd A pointer to the `cmdline` structure that stores all command line values.
+ * 
+ * @return void
+**/
+void parsecmdline(int argc, char *argv[], struct cmdline *cmd)
+{
+    int c = -1;
+
+    while (optind < argc)
+    {
+        if ((c = getopt_long(argc, argv, "c:zhvl", long_opts, NULL)) != -1)
+        {
+            switch (c)
+            {
+                case 'c':
+                    cmd->config = optarg;
+
+                    break;
+
+                case 'z':
+                    cmd->cli = 1;
+
+                    break;
+
+                /* CLI options. */
+                case 0:
+                    cmd->cl_interface = optarg;
+
+                    break;
+
+                case 2:
+                    cmd->cl_block = atoi(optarg);
+
+                    break;
+
+                case 3:
+                {
+                    char *val = strdup(optarg);
+                    cmd->cl_count = strtoull((const char *)val, (char **)val, 0);
+
+                    break;
+                }
+
+                case 4:
+                {
+                    char *val = strdup(optarg);
+                    cmd->cl_time = strtoull((const char *)val, (char **)val, 0);
+
+                    break;
+                }
+
+                case 5:
+                {
+                    char *val = strdup(optarg);
+                    cmd->cl_delay = strtoull((const char *)val, (char **)val, 0);
+
+                    break;
+                }
+
+                case 6:
+                    cmd->cl_track_count = atoi(optarg);
+
+                    break;
+
+                case 7:
+                {
+                    char *val = strdup(optarg);
+                    cmd->cl_data = strtoull((const char *)val, (char **)val, 0);
+
+                    break;
+                }
+
+                case 8:
+                    cmd->cl_threads = atoi(optarg);
+
+                    break;
+
+                case 9:
+                    cmd->cl_l4_csum = atoi(optarg);
+
+                    break;
+
+                case 10:
+                    cmd->cl_src_mac = optarg;
+
+                    break;
+
+                case 11:
+                    cmd->cl_dst_mac = optarg;
+
+                    break;
+
+                case 12:
+                    cmd->cl_ttl_min = atoi(optarg);
+
+                    break;
+
+                case 13:
+                    cmd->cl_ttl_max = atoi(optarg);
+
+                    break;
+
+                case 14:
+                    cmd->cl_id_min = atoi(optarg);
+
+                    break;
+
+                case 15:
+                    cmd->cl_id_max = atoi(optarg);
+
+                    break;
+
+                case 16:
+                    cmd->cl_src_ip = optarg;
+
+                    break;
+
+                case 17:
+                    cmd->cl_dst_ip = optarg;
+
+                    break;
+
+                case 18:
+                    cmd->cl_protocol = optarg;
+
+                    break;
+
+                case 19:
+                    cmd->cl_tos = atoi(optarg);
+
+                    break;
+
+                case 20:
+                    cmd->cl_l3_csum = atoi(optarg);
+
+                    break;
+
+                case 21:
+                    cmd->cl_udp_src_port = atoi(optarg);
+
+                    break;
+
+                case 22:
+                    cmd->cl_udp_dst_port = atoi(optarg);
+
+                    break;
+
+                case 23:
+                    cmd->cl_tcp_src_port = atoi(optarg);
+
+                    break;
+
+                case 24:
+                    cmd->cl_tcp_dst_port = atoi(optarg);
+
+                    break;
+
+                case 25:
+                    cmd->cltcpsyn = atoi(optarg);
+
+                    break;
+
+                case 26:
+                    cmd->cl_tcp_ack = atoi(optarg);
+
+                    break;
+
+                case 27:
+                    cmd->cl_tcp_psh = atoi(optarg);
+
+                    break;
+
+                case 28:
+                    cmd->cl_tcp_rst = atoi(optarg);
+
+                    break;
+
+                case 29:
+                    cmd->cl_tcp_fin = atoi(optarg);
+
+                    break;
+
+                case 30:
+                    cmd->cl_tcp_urg = atoi(optarg);
+
+                    break;
+
+                case 31:
+                    cmd->cl_tcp_use_socket = atoi(optarg);
+
+                    break;
+
+                case 32:
+                    cmd->cl_pl_min_len = atoi(optarg);
+
+                    break;
+
+                case 33:
+                    cmd->cl_pl_max_len = atoi(optarg);
+
+                    break;
+
+                case 34:
+                    cmd->cl_pl_is_static = atoi(optarg);
+
+                    break;
+
+                case 35:
+                    cmd->cl_pl_exact = optarg;
+
+                    break;
+
+                case 36:
+                    cmd->cl_pl_is_file = atoi(optarg);
+
+                    break;
+
+                case 37:
+                    cmd->cl_pl_is_string = atoi(optarg);
+
+                    break;
+					
+                case 38:
+                    cmd->cl_icmp_code = atoi(optarg);
+
+                    break;
+					
+                case 39:
+                    cmd->cl_icmp_type = atoi(optarg);
+
+                    break;
+
+                case 'l':
+                    cmd->list = 1;
+
+                    break;
+
+                case 'v':
+                    cmd->verbose = 1;
+
+                    break;
+
+                case 'h':
+                    cmd->help = 1;
+
+                    break;
+
+                case '?':
+                    fprintf(stderr, "Missing argument.\n");
+
+                    break;
+            }
+        }
+        else
+        {
+            optind++;
+        }
+    }
+}
---
+++ src/cmd_line.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <linux/types.h>
+#include "config.h"
+
+struct cmdline
+{
+    const char *config;
+    unsigned int list : 1;
+    unsigned int verbose : 1;
+    unsigned int help : 1;
+    unsigned int cli : 1;
+
+    /* Sequence options. */
+    char *cl_interface;
+    unsigned int cl_block : 1;
+    __u64 cl_count;
+    __u64 cl_time;
+    __u64 cl_delay;
+    __u64 cl_max_data;
+    unsigned int cl_track_count : 1;
+    __u16 cl_threads;
+    unsigned int cl_l4_csum : 1;
+
+    char *cl_src_mac;
+    char *cl_dst_mac;
+
+    __u16 cl_ttl_min;
+    __u16 cl_ttl_max;
+    __u16 cl_id_min;
+    __u16 cl_id_max;
+    char *cl_src_ip;
+    char *cl_dst_ip;
+    char *cl_protocol;
+    __u8 cl_tos;
+    unsigned int cl_l3_csum : 1;
+
+    __u16 cl_udp_src_port;
+    __u16 cl_udp_dst_port;
+
+    __u16 cl_tcp_src_port;
+    __u16 cl_tcp_dst_port;
+    unsigned int cl_tcp_syn : 1;
+    unsigned int cl_tcp_ack : 1;
+    unsigned int cl_tcp_psh : 1;
+    unsigned int cl_tcp_rst : 1;
+    unsigned int cl_tcp_fin : 1;
+    unsigned int cl_tcp_urg : 1;
+    unsigned int cl_tcp_use_socket : 1;
+
+    __u8 cl_icmp_code : 1;
+    __u8 cl_icmp_type : 1;
+
+    __u16 cl_pl_min_len;
+    __u16 cl_pl_max_len;
+    unsigned int cl_pl_is_static : 1;
+    char *cl_pl_exact;
+    unsigned int cl_pl_is_file : 1;
+    unsigned int cl_pl_is_string : 1;
+};
+
+void parsecmdline(int argc, char *argv[], struct cmdline *cmd);
+void parsecli(struct cmdline *cmd, struct config *cfg);
