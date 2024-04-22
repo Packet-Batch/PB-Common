35e460181483c86feb86faa83f9784285b640849
Christian Deacon <christianmdeacon@gmail.com>
2024-04-22 15:45:01 -0400

    Add get_src_mac_address() to config.h.

--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,7 @@
 #include <linux/types.h>
 
 void get_gw_mac(__u8 *mac);
+int get_src_mac_address(const char *dev, __u8 *src_mac);
 __u16 rand_num(__u16 min, __u16 max, unsigned int seed);
 char *lower_str(char *str);
 char *rand_ip(char *range, __u64 *pcktcount);
